5cbdcfd0091334763028392929e243bd50a5e531
João Marques <jonny1995@live.com.pt>
2019-01-10 23:02:13 +0000

    boteos check e model tambem

--- js/jacket.js
+++ js/jacket.js
@@ -9,6 +9,7 @@ let controls;
 let clock;
 let mixer;
 let clipes;
+let clipesRotacao;
 let cQuaternion;
 
 init();
@@ -17,28 +18,27 @@ animate();
 function init() {
   let axes;
   let loader;
+  var acaoMover = null;
 
   clipes = [];
+  clipesRotacao = [];
 
   scene = new THREE.Scene();
   let container = document.getElementById("container");
   console.log($(container).width(), $(container).height());
-  camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-  console.log(cQuaternion);
+  camera = new THREE.PerspectiveCamera(90, 1, 0.1, 1000);
 
-  /* camera.rotation.x = -0.2709125931542197;
-  camera.rotation.y = 0.022898195735745287;
-  camera.rotation.z = 0.006359123509105542;
-  camera.position.x = -0.25449088706444156;
-  camera.position.y = 2.3883640065182763;
-  camera.position.z = 4.388287524717485;*/
+  camera.rotation.x = -0.3993032695657877;
+  camera.rotation.y = 0.0022075378297279832;
+  camera.rotation.z = 0.0009315185389924363;
 
-  camera.rotation.x = -0.3342786257355676;
-  camera.rotation.y = -0.09721436885663409;
-  camera.rotation.z = -0.033697850248518686;
-  camera.position.x = -1.0149765517543652;
-  camera.position.y = 2.2210741065477464;
-  camera.position.z = 4.915826918820327;
+  camera.position.x = 0.013486243587948504;
+  camera.position.y = 2.3751014441331466;
+  camera.position.z = 5.62857344526669;
+
+
+
+
 
   /*camera.lookAt(0, 0, 0);
     /*camera.position.copy(cPosition);*/
@@ -57,27 +57,25 @@ function init() {
   renderer.setSize($(container).width(), $(container).width());
   renderer.shadowMap.enabled = true;
   scene.add(camera);
-  /*document.body.style.overflow = "hidden";*/
 
-  /*axes = new THREE.AxesHelper();
-    scene.add(axes);*/
+  axes = new THREE.AxesHelper();
+  //scene.add(axes);
+
+  var grelha = new THREE.GridHelper();
+  //scene.add(grelha);
 
   clock = new THREE.Clock();
   mixer = new THREE.AnimationMixer(scene);
 
   loader = new THREE.GLTFLoader();
-  loader.load("../models/Jacket.gltf", function(gltf) {
+  loader.load("../models/Jacket_anim_rotation.gltf", function (gltf) {
     scene.add(gltf.scene);
-
-    scene.traverse(function(x) {
+    scene.traverse(function (x) {
       if (x.isMesh) {
+        gltf.scene.position.set(0, -1.3, 0);
         x.castShadow = true;
         x.receiveShadow = true;
-        //x.position.x = -2;
-        // x.position.y = -6;
-        // x.position.z = 2;
-        // x.position.y = -6;
-        x.scale.set(4, 4, 4);
+        x.scale.set(7, 7, 7);
         //console.log(x);
       }
     });
@@ -87,9 +85,26 @@ function init() {
       mixer.clipAction(clipe).paused = false;
       mixer.clipAction(clipe).timeScale = 1;
     });
+
+    clipesRotacao.push(THREE.AnimationClip.findByName(gltf.animations, "rotacao"));
+    clipesRotacao.forEach(clipe => {
+      //mixer.clipAction(clipe).play();
+      mixer.clipAction(clipe).paused = false;
+      mixer.clipAction(clipe).timeScale = 1;
+      acaoMover = mixer.clipAction(clipe);
+    });
   });
 
+  document.getElementById('buttonPlay').onclick = function () {
+    acaoMover.play()
+  }
+
+  document.getElementById('buttonPause').onclick = function () {
+    acaoMover.paused = !acaoMover.paused
+  }
+
   controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enableZoom = false;
   /*controls.target.set(3, 0, -3);
     controls.update();*/
 
